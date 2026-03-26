a7686e7236a6d861c4b1f37c6f5135725127f2be
justnothing-h <harshit.choudhary0715@gmail.com>
2026-03-26 16:11:10 +0530

    updated

---
+++ printdigit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class printdigit {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the digit");
+        int n = sc.nextInt();
+        int count= 0;
+        int temp = n;
+
+        while (temp!=0){
+            temp= temp/10;
+            count ++;
+        }
+        int div = (int)Math.pow(10,count-1);
+        while (div!=0){
+            int q = n / div;
+            System.out.println(q);
+            n=n%div;
+            div = div/10;
+        }
+    }
+}

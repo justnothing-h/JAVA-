e61e029b32de642f10f94b02ab883e0e5f9245ff
justnothing-h <harshit.choudhary0715@gmail.com>
2026-03-27 21:38:29 +0530

    updated

---
+++ frequencyofnumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class frequencyofnumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int[] freq= new int[10];
+        while(n>0){
+            int digit = n%10;
+            freq[digit]++;
+            n= n / 10;
+        }
+        System.out.println("\nDigit Frequency:");
+        for(int i =0; i<10; i++){
+            if(freq[i]>0){
+                System.out.println(i+ "Occours"+ freq[i] + "times");
+            }
+        }
+    }
+}

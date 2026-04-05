835cfa62b5c3cfdc426aac3a429b41aec72a1f1e
justnothing-h <harshit.choudhary0715@gmail.com>
2026-04-05 20:10:45 +0530

    updated

--- primefactorisation.java
+++ primefactorisation.java
@@ -7,7 +7,7 @@ public class primefactorisation {
         System.out.println("Enter the number");
         int n = sc.nextInt();
         for (int div = 2; div*div<=n; div++){
-            while(n%div==0){
+            while(n%div==0){    // if the number is divisible by div, then it is a prime factor
                 n = n/div;
                 System.out.print(div+" ");
             }
